21bbb05c51c0bba00b3054ae9cc7325b5f5e9a56
Hema Sree Rathnam Machha <machhagusus21069@gmail.com>
2022-01-27 23:30:15 -0600

    Added Edit User models

---
+++ HandIn-HandOut Project File/HandInHandOut/HandInHandOut/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandInHandOut.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public EditUserViewModel()
+        {
+            Claims = new List<string>();
+            Roles = new List<string>();
+        }
+
+
+
+        public string Id { get; set; }
+
+
+
+        [Required]
+        public string UserName { get; set; }
+
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+        public string AdressLine1 { get; set; }
+        public string AdressLine2 { get; set; }
+        public string City { get; set; }
+
+
+
+        public string State { get; set; }
+
+
+
+
+
+        public List<string> Claims { get; set; }
+
+
+
+        public IList<string> Roles { get; set; }
+    }
+}
